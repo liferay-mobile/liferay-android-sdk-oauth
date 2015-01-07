bca92fd892754dc1ece45422746e9d9ea6304215
Bruno Farache <bruno.farache@liferay.com>
2015-01-07 18:58:27 -0300

    Reset webview url before second login attempt

--- sample-webview/src/main/java/com/liferay/mobile/sample/activity/MainActivity.java
+++ sample-webview/src/main/java/com/liferay/mobile/sample/activity/MainActivity.java
@@ -85,6 +85,7 @@ public class MainActivity extends Activity implements OAuthCallback {
 
 			@Override
 			public void onClick(View view) {
+				_webView.loadUrl("about:blank");
 				_webView.setVisibility(View.VISIBLE);
 				_webView.start(config, MainActivity.this);
 			}
