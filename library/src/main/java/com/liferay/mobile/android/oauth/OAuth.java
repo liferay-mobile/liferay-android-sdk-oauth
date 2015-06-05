a1866d6e067b6aaad4da111a7dc5f6405519e0ce
Bruno Farache <bruno.farache@liferay.com>
2015-06-05 10:38:41 -0300

    Add _config getter

--- library/src/main/java/com/liferay/mobile/android/oauth/OAuth.java
+++ library/src/main/java/com/liferay/mobile/android/oauth/OAuth.java
@@ -46,20 +46,8 @@ public class OAuth implements Authentication {
 		consumer.sign(request);
 	}
 
-	public String getConsumerKey() {
-		return _config.getConsumerKey();
-	}
-
-	public String getConsumerSecret() {
-		return _config.getConsumerSecret();
-	}
-
-	public String getToken {
-		return _config.getToken();
-	}
-
-	public String getTokenSecret) {
-		return _config.getTokenSecret();
+	public OAuthConfig getConfig() {
+		return _config;
 	}
 
 	private OAuthConfig _config;
